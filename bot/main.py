b1d26e4009ac00503440aac8b779c998ebdeff0d
Kremogen <nik.kosachev@mail.ru>
2023-02-18 23:26:20 +0300

    added Telegram-chat bot helper

---
+++ bot/main.py
@@ -0,0 +1,142 @@
+import sqlite3
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
+import config
+
+bot = Bot(token=config.API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+class Form(StatesGroup):
+    question = State()
+    answer = State()
+    category = State()
+    key_words = State()
+
+
+# Хэндлер стартовой команды
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute('SELECT id FROM users WHERE id = ?', (message.from_user.id,))
+    id = cursor.fetchone()
+    conn.close()
+    if id is None:
+        await create_user_profile(message.from_user.id)
+        await message.answer(
+            text=f'🤖 Привет, я специально обученный Бот, который поможет добавить новые вопросы в Базу Данных с вопросами!\n\nЧтобы добавить новый вопрос воспользуйся командой /add_question')
+    else:
+        await message.answer(
+            text=f'📚 Чтобы добавить новый вопрос воспользуйся командой /add_question')
+
+
+@dp.message_handler(commands=['add_question'])
+async def add_question(message: types.Message):
+    if message.from_user.id in config.ADMINS:
+        await Form.question.set()
+        await message.answer(text=f'🤖 Отправь вопрос, который хочешь добавить в Базу Данных:')
+    else:
+        await message.answer(text='🤖 К сожалению, ты не можешь воспользоваться данной командой :c')
+
+
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='Отмена', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    await state.finish()
+    await message.reply('🤖 Ок')
+
+
+@dp.message_handler(state=Form.question)
+async def process_question(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['question'] = message.text
+
+    await Form.next()
+    await message.reply('🤖 Введи ответ на заданный вопрос:')
+
+
+@dp.message_handler(state=Form.answer)
+async def process_answer(message: types.Message, state: FSMContext):
+    await state.update_data(answer=message.text)
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Технический', 'Финансовый', 'Форма')
+    markup.add('Другое')
+
+    await Form.next()
+    await message.reply('🤖 Выбери категорию из предложенных на клавиатуре', reply_markup=markup)
+
+
+#@dp.message_handler(lambda message: message.text not in ['Технический', 'Финансовый', 'Форма', 'Другое'], state=Form.answer)
+#async def process_category_invalid(message: types.Message):
+    #return await message.reply('🤖 Не знаю такую категорию, выбери из предложенных')
+
+
+@dp.message_handler(state=Form.category)
+async def process_category(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['category'] = message.text
+        markup = types.ReplyKeyboardRemove()
+        await message.answer(text='🤖 Отправь ключевые слова через запятую, которые относятся к добавленному вопросу:', reply_markup=markup)
+
+
+@dp.message_handler(state=Form.key_words)
+async def process_key_words(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['key_words'] = message.text
+        await add_new_question(data['question'], data['answer'], data['category'], data['key_words'])
+        await message.answer(text='🤖 Вопрос был успешно добавлен!')
+
+        await state.finish()
+
+
+# Создание профиля пользователя
+async def create_user_profile(id: int) -> None:
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (id) VALUES (?)', [id])
+    conn.commit()
+    conn.close()
+
+
+async def add_new_question(question: str, answer: str, category: str, key_words: str) -> None:
+    conn = sqlite3.connect("questions.db")
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO Questions (question, answer, category, key_words) VALUES (?, ?, ?, ?)', [question, answer, category, key_words])
+    conn.commit()
+    conn.close()
+
+
+# иницаилизация БД пользователей
+def init_db():
+    try:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users')
+        cursor.close()
+        conn.close()
+        print('База данных найдена, начинаю работу!'.encode('utf-8'))
+    except Exception:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            'CREATE TABLE users (id INTEGER PRIMARY KEY NOT NULL)'
+        )
+        conn.commit()
+        conn.close()
+        print('База данных не найдена, создаю и начинаю работу!'.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    init_db()
+    executor.start_polling(dp, skip_updates=True)
--- db.py
+++ db.py
@@ -1,21 +1,21 @@
 import sqlite3
 
-conn = sqlite3.connect('questions.db')
+conn = sqlite3.connect('bot/questions.db')
 
 
 def init_db():
     try:
-        conn = sqlite3.connect('questions.db')
+        conn = sqlite3.connect('bot/questions.db')
         cursor = conn.cursor()
         cursor.execute('SELECT * DEOM users')
         cursor.close()
         conn.close()
         print('БД найдена')
     except Exception:
-        conn = sqlite3.connect('questions.db')
+        conn = sqlite3.connect('bot/questions.db')
         cursor = conn.cursor()
         cursor.execute(
-            'create table if not exists Questions(id INT PRIMARY KEY,question VARCHAR(255) NOT NULL,answer VARCHAR(255) NOT NULL,category VARCHAR(30) NOT NULL,age INT NOT NULL DEFAULT 1,key_words VARCHAR(255) NOT NULL,status VARCHAR(255) NOT NULL);')
+            'create table if not exists Questions(id INT PRIMARY KEY AUTOINCREMENT,question VARCHAR(255) NOT NULL,answer VARCHAR(255) NOT NULL,category VARCHAR(30) NOT NULL,count INT NOT NULL DEFAULT 1,key_words VARCHAR(255) NOT NULL,status VARCHAR(255) NOT NULL DEFAULT 0);')
         conn.commit()
         conn.close()
         print('БД не найдена, создаю новую')
